8451066ba1fd2323869f1fd46dff02c96c24bfb9
ingglez89 <ingglez89@gmail.com>
2020-09-26 16:30:45 -0500

    second commit

--- index.js
+++ index.js
@@ -16,11 +16,18 @@ const port = process.env.PORT || "8000";
  app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
 /**
  * Routes Definitions
  */
  app.get("/", (req, res) => {
   res.status(200).send("WHATABYTE: Food For Devs");
+
+ app.get("/", (req, res) => {
+  res.render("index", { title: "Home" }); 
 /**
  * Server Activation
  */
